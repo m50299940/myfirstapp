f6db13ac2615c6222d279067de94bca7d9ddf9f3
m50299940 <134176818+m50299940@users.noreply.github.com>
2023-05-28 11:15:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,34 @@ var http = require("http").Server(app);
 app.use(express.static("public"));
 
 http.listen(3000, function () {
-    console.log("Server gestartet, listing on localhost:3000");
+    get("Server gestartet, listing on localhost:3000");
+    get();
 });
 
 app.get("/", function (req, res) {
-    
-    console.log("yes");
 
-    res.sendFile(__dirname + "/index.html");
+    get("yes");
+
+
+
+    res.write(get("Majd"));
+    res.end();
 });
+
+function get(params) {
+    console.log(params);
+
+    //console.log(typeof (params));
+
+    const { join } = require('path');
+
+    const fromPath = join(__dirname, "from", "move-me.txt");
+    const destPath = join(__dirname, "to", "move-me.txt");
+
+    console.log(fromPath);
+    console.log(destPath);
+
+    console.log("dkfjlöak");
+
+    return "<h1>" + params + "</h1>";
+}
