c565a4f1413bde99c884316c5460c8611c76b6d4
m50299940 <134176818+m50299940@users.noreply.github.com>
2023-05-29 22:48:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,27 +6,12 @@ var http = require("http").Server(app);
 app.use(express.static("public"));
 
 http.listen(3000, function () {
-    get("Server gestartet, listing on localhost:3000");
-    get();
+    console.log("Server gestartet, listing on localhost:3000");
 });
 
 app.get("/", function (req, res) {
-
-    get("yes");
-
-
-
-    res.write(get("Majd"));
-    res.write(get(process.memoryUsage()["heapUsed"]));
-    res.write(get(process.memoryUsage()["heapTotal"]));
-    res.write(get(__dirname));
-    res.write(get(__filename));
-    res.write(get(process.env.WebStorm));
+    directoryContent = fs.readdirSync(__dirname);
+    console.log(directoryContent);
+    res.write(directoryContent);
     res.end();
 });
-
-function get(params) {
-    console.log(params);
-
-    return "<h1>" + params + "</h1>";
-}
