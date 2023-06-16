171b133aba9c93bea288a24b9b6865c440c4f288
m50299940 <134176818+m50299940@users.noreply.github.com>
2023-06-16 18:30:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,35 +14,3 @@ app.get("/", function (req, res) {
     console.log(directoryContent);
     res.send(directoryContent);
 });
-
-console.log("nach");
-const path = "./config.json"
-const config = { ip: "10.10.10.10", port: 3000 };
-console.log(config);
-console.log(JSON.stringify(config));
-console.log(JSON.parse(JSON.stringify(config)));
-console.log(path, config);
-fs.writeFile(path, JSON.stringify(config, null, 2), (error) => {
-    if (error) {
-        console.log("Nooooooooooooooooooo", error);
-        return;
-    }
-    console.log("yyyyyyyyyyyyyyyyyyyyyes");
-});
-
-
-
-function fib(n) {
-    counter += 1
-    if (n <= 1) {
-        return n
-    }
-    return fib(n - 1) + fib(n - 2)
-}
-
-console.log("number \t fib \t aufrufe");
-counter = 0
-for (let index = 0; index < 50; index++) {
-    counter = 0
-    console.log(index + "\t" + fib(index) + "\t" + counter);
-}
